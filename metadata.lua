706945bfa697c6095ef9fa0cf9d3487763be374e
Janeene Beeforth <gh@dawnmist.net>
2018-11-25 21:34:20 +1100

    Updated steam version.

--- metadata.lua
+++ metadata.lua
@@ -4,7 +4,7 @@ return PlaceObj('ModDef', {
 	'steam_id', "1574069378",
 	'image', "Preview.png",
 	'author', "Dawnmist",
-	'version', 5,
+	'version', 6,
 	'lua_revision', 237920,
 	'code', {"Code/VIPTracker.lua"},
 	'saved', 1543110572,
