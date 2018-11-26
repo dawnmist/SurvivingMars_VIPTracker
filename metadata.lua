115df80b86092ef629a7c26c8b1ce8cd69785e64
Janeene Beeforth <gh@dawnmist.net>
2018-11-26 13:45:15 +1100

    Add steam description for the mod.

--- metadata.lua
+++ metadata.lua
@@ -3,13 +3,33 @@ return PlaceObj('ModDef', {
 	'id', "Dawnmist_VIPTracker",
 	'steam_id', "1574069378",
 	'image', "Preview.png",
-	'last_changes', "Improve compatibility with other mods that add new death reasons",
+	'last_changes', "Add Steam mod description",
 	'author', "Dawnmist",
-	'version', 7,
+	'version', 9,
 	'lua_revision', 237920,
 	'code', {"Code/VIPTracker.lua"},
 	'saved', 1543110572,
 	'TagInterface', true,
 	'TagTools', true,
 	'TagOther', true,
+	'description', [[
+[h1]VIP Tracker[/h1]
+
+This mod is Gagarin compatible (both base game and with Space Race).
+
+This mod enables you to mark individual colonists as ones that you wish to track. These colonists are given a Quirk called "VIP" so that they can be listed in the Colony Command Center colonist filters. Double-clicking on a colonist in this colonist list will zoom you to where they are.
+
+When a VIP dies, the mod keeps track of their age at death, the Sol they died, and what their cause of death was. A notification is popped up that allows you to zoom to the dead VIP. An additional option is also added to the Colony Command Center to view the list of deceased VIPs.
+
+[h1]Acknowledgements[/h1]
+
+The original idea for this mod came from CheTranqui with the mod "Neighbourhood Watch". VIP Tracker is a complete rewrite of the ideas from "Neighbourhood Watch" made with CheTranqui's blessing.
+
+Other people that helped with working out how to make parts of the mod work include ChoGGi and SkiRich, plus others in the Surviving Mars Modding Discord.
+
+[h1]Source and Bug Reports[/h1]
+
+VIP Tracker is hosted at Github at https://github.com/dawnmist/SurvivingMars_VIPTracker and bug reports can also be created at Github.
+VIP Tracker has also been published to NexusMods at https://www.nexusmods.com/survivingmars/mods/99
+	]]
 })
